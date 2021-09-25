b752ce4dd540c0e3f6904274688efc334cdd7560
Ferdie Pendon <fcpendon@gmail.com>
2021-09-26 05:26:39 +0800

    Added loading spinner when fetching data

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import loadingSpinner from '../React-icon.svg';
+
+const LoadingSpinner = () => {
+  return <img src={loadingSpinner} alt="loading-spinner" className="loading-spinner"></img>;
+}
+
+export default LoadingSpinner;
